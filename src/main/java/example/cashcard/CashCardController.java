eaed3539e018e77afd2d2847de8dfdfb5011646c
Alexander Kramer <a.kramer@reply.de>
2023-02-03 13:31:46 +0100

    :sparkles: get cash card by id

---
+++ src/main/java/example/cashcard/CashCardController.java
@@ -0,0 +1,17 @@
+package example.cashcard;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cashcards")
+public class CashCardController {
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CashCard> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(new CashCard(id, 123.45));
+    }
+}
---
+++ src/test/java/example/cashcard/CashCardApplicationTest.java
@@ -0,0 +1,34 @@
+package example.cashcard;
+
+import com.jayway.jsonpath.JsonPath;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CashCardApplicationTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void shouldReturnACashCardWhenDataIsSaved() {
+        // when
+        var response = restTemplate.getForEntity("/cashcards/99", String.class);
+
+        // then
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        var documentContext = JsonPath.parse(response.getBody());
+        Number id = documentContext.read("$.id");
+        Double amount = documentContext.read("$.amount");
+        assertThat(id).isNotNull();
+        assertThat(id).isEqualTo(99);
+        assertThat(amount).isNotNull();
+        assertThat(amount).isEqualTo(123.45);
+    }
+}
